8867d1f2a02ff63552597aab5f3fc2eb76e1245c
Erik Flores <erikfloresq@icloud.com>
2021-10-21 21:55:14 -0300

    Initial Commit

---
+++ CameraApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CameraApp
+//
+//  Created by Erik Flores on 21/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
